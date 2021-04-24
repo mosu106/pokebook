48ba122e5460037528922543ee5575db643b1cd9
mosu106 <rsunaga7@gmail.com>
2021-04-24 09:48:06 +0900

    yamlファイルの修正

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const path = require('path')
-const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  assetPrefix: isProd ? 'https://mosu106.github.io/pokebook' : '',
+
+  assetPrefix: process.env.GITHUB_PAGES ? '/pokebook' : '.',
   webpack(config) {
     config.resolve.alias = {
       ...config.resolve.alias,
